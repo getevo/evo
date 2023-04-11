af33fd5d382f051dfdaf4f1ef025c305598d548b
RezaPC <reza@ies-italia.it>
2023-04-11 18:00:45 +0200

    build

--- lib/build/build.go
+++ lib/build/build.go
@@ -1,7 +1,10 @@
 package build
 
 // build with -ldflags="-X 'github.com/getevo/evo/v2/lib/build.Version=v1.0.0' -X 'github.com/getevo/evo/v2/lib//build.User=$(id -u -n)' -X 'github.com/getevo/evo/v2/lib/build.Time=$(date)' -X 'github.com/getevo/evo/v2/lib/build.Commit=e21cf23'"
-import "fmt"
+import (
+	"fmt"
+	"github.com/getevo/evo/v2"
+)
 
 var Version = "v0.0.1-dev"
 var User = ""
@@ -10,4 +13,6 @@ var Commit = ""
 
 func Register() {
 	fmt.Println("Build", "Version:", Version, "|", "User:", User, "|", "Date:", Date, "|", "Commit:", Commit)
+
+	evo.Get("/version")
 }
